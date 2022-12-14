4263d6e635d07fd7f7bc923c4b2b533d202cb7c1
Patrick <96917554+patrickzondervan@users.noreply.github.com>
2022-12-14 07:03:24 +0100

    add some more nbt parsing code

--- decode/src/nbt.rs
+++ decode/src/nbt.rs
@@ -6,6 +6,7 @@ use serde::{Deserialize, Serialize};
 use std::{
     collections::HashMap,
     io::{BufRead, BufReader},
+    num::TryFromIntError,
 };
 
 #[derive(Debug, Serialize, Deserialize, NBTEncoder, NBTDecoder)]
@@ -79,11 +80,44 @@ where
             value if value.starts_with('"') && value.ends_with('"') => {
                 Value::String(String::from(&value[1..value.len() - 1]))
             }
-            value if value.chars().all(|c| c.is_digit(10)) => Value::Int(
-                value
-                    .parse::<i32>()
-                    .map_err(|e| std::io::Error::new(std::io::ErrorKind::InvalidData, e))?,
-            ),
+            value if value.starts_with('[') && value.ends_with(']') => {
+                fn parse_value_array<T>(value: &str) -> Result<Vec<T>, std::io::Error>
+                where
+                    T: std::str::FromStr,
+                    <T as std::str::FromStr>::Err: std::fmt::Debug,
+                {
+                    value[1..value.len() - 1]
+                        .split(",")
+                        .map(|s| s.trim().parse())
+                        .map(|s| {
+                            s.map_err(|e| {
+                                std::io::Error::new(
+                                    std::io::ErrorKind::Unsupported,
+                                    format!("{:?}", e),
+                                )
+                            })
+                        })
+                        .collect()
+                }
+
+                if let Ok(value) = parse_value_array::<i8>(value) {
+                    Value::ByteArray(value)
+                } else if let Ok(value) = parse_value_array::<i32>(value) {
+                    Value::IntArray(value)
+                } else if let Ok(value) = parse_value_array::<i64>(value) {
+                    Value::LongArray(value)
+                } else {
+                    return Err(
+                        std::io::Error::new(std::io::ErrorKind::Unsupported, "Unsupported operation, not able to match i8, i32 or i64 in Array NBT parsing.")
+                    )
+                }
+            }
+            value if value.parse::<i8>().is_ok() => Value::Byte(value.parse::<i8>().unwrap()),
+            value if value.parse::<i16>().is_ok() => Value::Short(value.parse::<i16>().unwrap()),
+            value if value.parse::<i32>().is_ok() => Value::Int(value.parse::<i32>().unwrap()),
+            value if value.parse::<i64>().is_ok() => Value::Long(value.parse::<i64>().unwrap()),
+            value if value.parse::<f32>().is_ok() => Value::Float(value.parse::<f32>().unwrap()),
+            value if value.parse::<f64>().is_ok() => Value::Double(value.parse::<f64>().unwrap()),
             _ => {
                 return Err(std::io::Error::new(
                     std::io::ErrorKind::Unsupported,
